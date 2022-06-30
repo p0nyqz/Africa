b21effccc7a0f54ed972b9fa3f22998644f576f8
p0nyqz <ponyquiz@gmail.com>
2022-06-30 23:41:34 +0200

    CodableBundleExtension implemented

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by p0nyqz on 30.06.2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
