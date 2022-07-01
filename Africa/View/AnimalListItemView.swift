0ee1dade3dadcc23347f153891701651f37d5f00
p0nyqz <ponyquiz@gmail.com>
2022-07-02 00:17:33 +0200

    Implement Swift Generics

--- Africa/View/AnimalListItemView.swift
+++ Africa/View/AnimalListItemView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct AnimalListItemView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
     var body: some View {
         HStack(alignment: .center, spacing: 16) {
-            Image("lion")
+            Image(animal.image)
                 .resizable()
                 .scaledToFill()
                 .frame(width: 90, height: 90)
@@ -18,11 +22,11 @@ struct AnimalListItemView: View {
                     RoundedRectangle(cornerRadius: 12)
                 )
             VStack(alignment: .leading, spacing: 8) {
-                Text("Lion")
+                Text(animal.name)
                     .font(.title2)
                     .fontWeight(.heavy)
                     .foregroundColor(.accentColor)
-                Text("The world's most social felines, lions roam the savannas and grasslands of the African continent, hunting cooperatively and raising cubs in prides.")
+                Text(animal.headline)
                     .font(.footnote)
                     .multilineTextAlignment(.leading)
                     .lineLimit(2)
@@ -33,8 +37,9 @@ struct AnimalListItemView: View {
 }
 
 struct AnimalListItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
     static var previews: some View {
-        AnimalListItemView()
+        AnimalListItemView(animal: animals[1])
             .previewLayout(.sizeThatFits)
             .padding()
     }
