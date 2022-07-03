e362a6c51f4c6cb9497d8298f9a404377ea8db27
p0nyqz <ponyquiz@gmail.com>
2022-07-03 23:46:56 +0200

    feat: HeadingView: Created reusable header component

---
+++ Africa/View/HeadingView.swift
@@ -0,0 +1,39 @@
+//
+//  HeadingView.swift
+//  Africa
+//
+//  Created by p0nyqz on 03.07.2022.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    
+    // MARK: - PROPERTIES
+    
+    var headingImage: String
+    var headingText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: headingImage)
+                .foregroundColor(.accentColor)
+                .imageScale(.large)
+            
+            Text(headingText)
+                .font(.title3)
+                .fontWeight(.bold)
+        }
+        .padding(.vertical)
+    }
+}
+
+
+// MARK: - PREVIEW
+struct HeadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
