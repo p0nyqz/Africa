01d59250c8830aec327c28b5f852a2344bffe88b
p0nyqz <ponyquiz@gmail.com>
2022-07-04 00:27:53 +0200

    feat: ContentView, AnimalDetailView: Link binding

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -20,7 +20,10 @@ struct ContentView: View {
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                 
-                ForEach(animals) {animal in AnimalListItemView(animal: animal)}
+                ForEach(animals) {animal in
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimalListItemView(animal: animal)}                        
+                    } //: LINK
             } //: LIST
             .navigationBarTitle("Africa", displayMode: .large)
         } //: NAVIGATION
--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -45,6 +45,8 @@ struct AnimalDetailView: View {
                 Group {
                     HeadingView(headingImage: "photo.on.rectangle.angled",
                                 headingText: "Wilderness in Pictures")
+                    
+                    InsetGalleryView(animal: animal)
                 }
                 .padding(.horizontal)
                 
