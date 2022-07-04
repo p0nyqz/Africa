87b16aa957c3a0ecdd885095d353a1ac384bf29e
p0nyqz <ponyquiz@gmail.com>
2022-07-05 00:20:41 +0200

    feat:InsetMapView: MapKit Implemented

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -70,8 +70,11 @@ struct AnimalDetailView: View {
                 // MAP
                 Group {
                     HeadingView(headingImage: "map", headingText: "National Parks")
-                
+                    
+                    InsetMapView()
                 }.padding(.horizontal)
+                
+
                 
                 // LINK
             } //: VSTACK
