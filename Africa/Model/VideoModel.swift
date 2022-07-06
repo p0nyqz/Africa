5de88e78e34fae683c6c394841e97add6d61b890
p0nyqz <ponyquiz@gmail.com>
2022-07-06 23:24:22 +0200

    feat:VideoModel, VideoListItem: video thumbnail component implemented

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by p0nyqz on 06.07.2022.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
---
+++ Africa/View/VideoListItem.swift
@@ -0,0 +1,59 @@
+//
+//  VideoListItem.swift
+//  Africa
+//
+//  Created by p0nyqz on 06.07.2022.
+//
+
+import SwiftUI
+
+struct VideoListItem: View {
+    // MARK: - PROPERTIES
+    
+    let video: Video
+    
+    // MARK: - BODY
+
+    var body: some View {
+        HStack(spacing: 10) {
+            ZStack {
+                Image("video=\(video.id)")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            } //: ZSTACK
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            } //: VSTACK
+        } //: HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+struct VideoListItem_Previews: PreviewProvider {
+    
+    static let videos: [Video] = Bundle.main.decode("video.json")
+    
+    static var previews: some View {
+        VideoListItem(video: videos[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
