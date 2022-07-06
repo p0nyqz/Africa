25062fdb376da8c413dcc33ad0696586808130c7
p0nyqz <ponyquiz@gmail.com>
2022-07-06 23:50:36 +0200

    feat:VideoListView: List of thumbnails added

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+    
+    var videos: [Video] = Bundle.main.decode("video.json")
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+        } //: NAVIGATION
     }
 }
+
+// MARK: - PREVIEW
 
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
