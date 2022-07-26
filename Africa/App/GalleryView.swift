5140b8afa3ef0e95ca0686beda83f0a876f2fdb6
p0nyqz <ponyquiz@gmail.com>
2022-07-26 21:32:33 +0200

    feat:galleryView: LazyVGrid

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // SIMPLE GRID DEFINITION
+    let gridLayout:[GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    // MARK: - BODY
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            // MARK: - GRID
+            
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                ForEach(animals) { item in
+                    Image(item.image)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                }//: LOOP
+            }//: GRID
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
+
+// MARK: - PREVIEW
 
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
