005ad276cf1828e47ba36808bf85cbffa25608a6
p0nyqz <ponyquiz@gmail.com>
2022-07-26 22:12:25 +0200

    fear:GelleryView: slider implemented

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -22,7 +22,16 @@ struct GalleryView: View {
     // ]
     
     // EFFICIENT GRID DEFINITION
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    // let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // DYNAMIC GRID LAYOUT
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch(){
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
     
     // MARK: - BODY
     
@@ -37,6 +46,14 @@ struct GalleryView: View {
                     .scaledToFit()
                     .clipShape(Circle())
                     .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                // MARK: - SLIDER
+                
+                Slider(value: $gridColumn, in: 2...4)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        gridSwitch()
+                    })
 
                 // MARK: - GRID
                 LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
@@ -51,6 +68,9 @@ struct GalleryView: View {
                             }
                     } //: LOOP
                 } //: GRID
+                onAppear(perform: {
+                    gridSwitch()
+                })
             } //: VSTACK
             .padding(.horizontal, 10)
             .padding(.vertical, 50)
