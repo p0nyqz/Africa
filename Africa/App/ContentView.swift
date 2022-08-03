a473cff50bc1a8a10fa5e07745bf597950161ed2
p0nyqz <ponyquiz@gmail.com>
2022-08-03 16:15:19 +0200

    feat:ContentView: buttons for switching layout added

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,25 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActivate: Bool = false
+    
     var body: some View {
-        // MARK: - PROPERTIES
-        
-        let animals: [Animal] = Bundle.main.decode("animals.json")
-        
         // MARK: - BODY
+        
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals) {animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
-                    }//: LINK
-                } //: LOOP
-            } //: LIST
+            Group {
+                NavigationView {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) {animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            }//: LINK
+                        } //: LOOP
+                    } //: LIST
+            } //: GROUP
+        }
+            
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // LIST
+                        Button(action: {
+                            print("List view is activated")
+                            isGridViewActivate = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActivate ? .primary : .accentColor)
+                        }
+                        // GRID
+                        Button(action: {
+                            print("Grid view is activated")
+                            isGridViewActivate = true
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActivate ? .accentColor : .primary)
+                        }
+                    } //: HSTACK
+                } //: BUTTONS
+            } //: TOOLBAR
         } //: NAVIGATION
     }
 }
